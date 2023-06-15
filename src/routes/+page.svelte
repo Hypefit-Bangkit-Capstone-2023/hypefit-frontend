ea4653ec069dadd8a5c9aa60b5a1b8548fc04f9b
Budha Lantara <budhalantara@gmail.com>
2023-06-15 21:03:52 +0800

    feat: handle no task, pending task, started task

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,31 +4,84 @@
 	import { isDataLoaded } from '$lib/stores/loading';
 
 	/**@type {import('$lib/services/types').RecommendationItem[]}*/
-	let items;
+	let items = [];
+
+	let isTaskNotCreated = false;
+	let isTaskPending = false;
+	let isTaskStarted = false;
+	let isRecommendationReady = false;
 
 	$isDataLoaded = false;
+
 	recommendationService.getRecommendation().then((res) => {
 		$isDataLoaded = true;
+
+		const { completed_task_count, failed_task_count, pending_task_count, started_task_count } =
+			res.data;
+
 		items = res.data.items;
+
+		if (
+			!items.length &&
+			!pending_task_count &&
+			!started_task_count &&
+			!completed_task_count &&
+			!failed_task_count
+		) {
+			isTaskNotCreated = true;
+		} else if (!items.length && pending_task_count) {
+			isTaskPending = true;
+		} else if (!items.length && started_task_count) {
+			isTaskStarted = true;
+		} else if (items.length && completed_task_count) {
+			isRecommendationReady = true;
+		}
 	});
 </script>
 
 {#if $isDataLoaded}
-	<div class="px-4 mt-4">
-		<div class="text-blue-3">
-			<h1 class="font-bold text-xl">Recommended for you</h1>
-			<p class="font-medium text-sm opacity-60">
-				Discover personalized recommendations tailored just for you, based on your unique
-				preferences and interests
-			</p>
+	{#if isTaskNotCreated}
+		<div class="h-full flex justify-center items-center mx-auto text-center px-4">
+			<div class=" text-md leading-6">
+				<h1 class="text-blue-3 font-bold">Done adding items to your wardrobe?</h1>
+				<h2 class="text-blue-2 font-medium">Get your tailored recommendations now!</h2>
+				<button
+					class="mt-2 uppercase text-xl text-blue-2 font-medium bg-blue-3 py-2 px-2 w-full rounded-md"
+				>
+					Tap Here
+				</button>
+			</div>
 		</div>
+	{/if}
 
-		<div class="grid mt-2 py-2 gap-4">
-			{#each items as item (item.id)}
-				<OutfitFeedCard id={item.id} imageUrls={item.image_urls} isLiked={item.is_liked} />
-			{/each}
+	{#if isTaskPending || isTaskStarted}
+		<div class="h-full flex justify-center items-center mx-auto text-center px-4">
+			<div class="font-bold text-blue-3 text-md leading-6">
+				<h1>We've got your recommendation request!</h1>
+				<h2 class="font-medium opacity-60">
+					Sit tight while we generate tailored recommendations just for you.
+				</h2>
+			</div>
 		</div>
-	</div>
+	{/if}
+
+	{#if isRecommendationReady}
+		<div class="px-4 mt-4">
+			<div class="text-blue-3">
+				<h1 class="font-bold text-xl">Recommended for you</h1>
+				<p class="font-medium text-sm opacity-60">
+					Discover personalized recommendations tailored just for you, based on your unique
+					preferences and interests
+				</p>
+			</div>
+
+			<div class="grid mt-2 py-2 gap-4">
+				{#each items as item (item.id)}
+					<OutfitFeedCard id={item.id} imageUrls={item.image_urls} isLiked={item.is_liked} />
+				{/each}
+			</div>
+		</div>
+	{/if}
 {/if}
 
 <style lang="postcss">
